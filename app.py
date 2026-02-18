c32db29d52a7e210b0a144839be822a6ba983794
Ethan Tobey <etobey04@gmail.com>
2026-02-17 22:27:34 -0500

    updated gemini package to current distribution

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-import google.generativeai as genai
+from google import genai
 from flask import Flask, request, jsonify
 import os
 from dotenv import load_dotenv
@@ -14,14 +14,15 @@ if not gemini_api_key:
     raise ValueError("GEMINI_API_KEY not found in environment variables")
 
 #initialize LLM model
-genai.configure(api_key=gemini_api_key)
-model = genai.GenerativeModel("gemini-2.5-flash")
+client = genai.Client(api_key=gemini_api_key)
 
 #basic helper function to make llm call
 #just returns text back for now
 def call_llm(prompt: str) -> str:
     # removed env var check cause we are hard coding for now
-    response = model.generate_content(prompt)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt)
 
     if not response.text:
         raise ValueError("Empty response from LLM")
