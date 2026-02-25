2e6fc1df62290121bf4e4fe4bdd39f61289c8085
Sreya-Srinidhi <sreyaksrinidhi@gmail.com>
2026-02-24 19:27:35 -0500

    established resume endpoint

---
+++ app/routes/resume_upload.py
@@ -0,0 +1,23 @@
+import os
+from flask import Blueprint, request
+from werkzeug.utils import secure_filename
+
+upload_bp = Blueprint("upload", __name__)
+
+UPLOAD_DIR = os.path.join(os.getcwd(), "uploads")
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+
+@upload_bp.route("/upload_resume", methods=["POST"])
+def upload_resume():
+    file = request.files.get("resume")
+
+    if not file or not file.filename:
+        return "", 400  # bad request
+
+    filename = secure_filename(file.filename)
+    save_path = os.path.join(UPLOAD_DIR, filename)
+
+    file.save(save_path)
+
+    return "", 204  # success, no content
